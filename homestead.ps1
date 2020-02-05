01c520b0fb5424ff2040084812de405838301201
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-02-05 01:53:19 +0100

    Improve code quality

--- homestead.ps1
+++ homestead.ps1
@@ -1,45 +1,41 @@
 $OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
 
-function RefreshPath {
-    $path = @(
-        [System.Environment]::GetEnvironmentVariable("Path", "Machine"),
-        [System.Environment]::GetEnvironmentVariable("Path", "User")
+function Use-NewPath {
+    $Path = @(
+        [System.Environment]::GetEnvironmentVariable('Path', 'Machine'),
+        [System.Environment]::GetEnvironmentVariable('Path', 'User')
     )
-    $env:Path = $path -join ";"
-                
+    $env:Path = $Path -join ';'
 }
 
-function Find($collection, [scriptblock]$predicate) {
-    foreach ($_ in $collection) {
-        if ((& $predicate) -eq $true) {
-            return $_
-        }
-    }
-
-    return $null
+function Find($Collection, [scriptblock] $Predicate) {
+    $Fn = [Func[object, bool]] { param($_) & $Predicate }
+    return [Linq.Enumerable]::FirstOrDefault($Collection, $Fn)
 }
 
-function WhereCommand([string]$command) {
-    $meta = Get-Command $command -ErrorAction Ignore
-    if ($null -eq $meta) {
-        return $null
+function Get-Document([string] $Uri) {
+    return Invoke-WebRequest $Uri -UseBasicParsing
+}
+
+function Install-IfNecessary([string] $Command, [string] $Name, [scriptblock] $Installer) {
+    if (Get-Command $Command -ErrorAction Ignore) {
+        Write-Output "$Name már telepítve van"
     }
     else {
-        return $meta.Path
+        & $Installer
     }
 }
 
 function Get-Downloader {
-    $downloader = New-Object System.Net.WebClient
-    $downloader.Credentials = [System.Net.CredentialCache]::DefaultCredentials
-    $downloader.Proxy = [System.Net.GlobalProxySelection]::GetEmptyWebProxy()
+    $Downloader = New-Object System.Net.WebClient
+    $Downloader.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+    $Downloader.Proxy = [System.Net.GlobalProxySelection]::GetEmptyWebProxy()
 
-    return $downloader
+    return $Downloader
 }
 
-function Download-File([string]$url, [string]$file) {
-    $downloader = Get-Downloader
-    $downloader.DownloadFile($url, $file)
+function Get-RemoteFile([string] $Url, [string] $Filename) {
+    (Get-Downloader).DownloadFile($Url, $Filename)
 }
 
 function Out-FileUtf8NoBom {
@@ -61,116 +57,102 @@ function Out-FileUtf8NoBom {
         Throw [IO.IOException] "The file '$LiteralPath' already exists."
     }
 
-    $sw = New-Object IO.StreamWriter $LiteralPath, $Append
+    $Sw = New-Object IO.StreamWriter $LiteralPath, $Append
   
-    $htOutStringArgs = @{ }
+    $OutStringArgs = @{ }
     if ($Width) {
-        $htOutStringArgs += @{ Width = $Width }
+        $OutStringArgs.Width = $Width
     }
 
     try {
-        $Input | Out-String -Stream @htOutStringArgs | % { $sw.WriteLine($_) }
+        $Input | Out-String -Stream @OutStringArgs | % { $Sw.WriteLine($_) }
     }
     finally {
-        $sw.Dispose()
+        $Sw.Dispose()
     }
 }
 
-if ($null -eq (WhereCommand choco)) {
-    Write-Host "Chocolatey telepítése"
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    RefreshPath
-}
-else {
-    Write-Host "Chocolatey már telepítve van"
+Install-IfNecessary choco Chocolatey {
+    Write-Host "$Name telepítése"
+    Invoke-Expression ((Get-Downloader).DownloadString('https://chocolatey.org/install.ps1'))
+    Use-NewPath
 }
 
-if ($null -eq (WhereCommand node)) {
-    Write-Host "Node telepítése"
+Install-IfNecessary node Node {
+    Write-Host "$Name telepítése"
     & choco install -y node
-    RefreshPath
-}
-else {
-    Write-Host "Node már telepítve van"
+    Use-NewPath
 }
 
-if ($null -eq (WhereCommand cmder)) {
-    Write-Host "cmder és UNIX eszközök telepítése"
+Install-IfNecessary cmder 'cmder és UNIX eszközök' {
+    Write-Host "$Name telepítése"
     & choco install -y cmder
-    RefreshPath
-}
-else {
-    Write-Host "cmder és UNIX eszközök már telepítve vannak"
+    Use-NewPath
 }
 
-if ($null -eq (WhereCommand vagrant)) {
-    Write-Host "Vagrant telepítése (ez sokáig eltarthat)"
-    $links = (Invoke-WebRequest "https://www.vagrantup.com/downloads.html" -UseBasicParsing).Links
-    $link = Find $links { $oh = [string]$_.outerHTML; $oh.Contains('data-os="windows"') -and $oh.Contains('data-arch="x86_64"') }
-    $href = $link.href
-    Download-File $href "vagrant.msi"
-    Start-Process msiexec -Wait -ArgumentList @("/I", (Resolve-Path "vagrant.msi"))
-    Remove-Item "vagrant.msi"
-    RefreshPath
-}
-else {
-    Write-Host "Vagrant már telepítve van"
+Install-IfNecessary vagrant Vagrant {
+    Write-Host "$Name telepítése (ez sokáig eltarthat)"
+    $Link = Find (Get-Document 'https://www.vagrantup.com/downloads.html').Links `
+    { $_.outerHTML.Contains('data-os="windows"') -and $_.outerHTML.Contains('data-arch="x86_64"') }
+    Get-RemoteFile $Link.href 'vagrant.msi'
+    Start-Process -Wait msiexec '/I', (Resolve-Path 'vagrant.msi')
+    Remove-Item 'vagrant.msi'
+    Use-NewPath
 }
 
-Write-Host "Hosts File Editor telepítése"
-Download-File "https://github.com/scottlerch/HostsFileEditor/releases/download/v1.2.0/HostsFileEditorSetup-1.2.0.msi" "hfe.msi"
-Start-Process msiexec -Wait -ArgumentList @("/I", (Resolve-Path "hfe.msi"))
-Remove-Item "hfe.msi"
+Write-Host 'Hosts File Editor telepítése'
+Get-RemoteFile 'https://github.com/scottlerch/HostsFileEditor/releases/download/v1.2.0/HostsFileEditorSetup-1.2.0.msi' 'hfe.msi'
+Start-Process -Wait msiexec '/I', (Resolve-Path 'hfe.msi')
+Remove-Item 'hfe.msi'
 
-$selectedVm = 0
-$vms = @(
+$SelectedVm = 0
+$Vms = @(
     @(
-        "VirtualBox",
+        'VirtualBox',
         {
-            $links = (Invoke-WebRequest "https://www.virtualbox.org/wiki/Downloads" -UseBasicParsing).Links
-            $link = Find $links { $_.class -eq "ext-link" }
-            $href = $link.href
-            Download-File $href "vbox.exe"
-            Start-Process vbox.exe -Wait
-            Remove-Item "vbox.exe"
+            $Link = Find (Get-Document 'https://www.virtualbox.org/wiki/Downloads').Links `
+            { $_.class -eq 'ext-link' }
+            Get-RemoteFile $Link.href 'vbox.exe'
+            Start-Process -Wait 'vbox.exe'
+            Remove-Item 'vbox.exe'
         }
     ),
     @(
-        "VMWare",
+        'VMWare',
         {
-            Download-File "https://www.vmware.com/go/getplayer-win" "vmware.exe"
-            Start-Process vmware.exe -Wait
-            Remove-Item "vmware.exe"
+            Get-RemoteFile 'https://www.vmware.com/go/getplayer-win' 'vmware.exe'
+            Start-Process -Wait 'vmware.exe'
+            Remove-Item 'vmware.exe'
         },
-        "vmware_workstation"
+        'vmware_workstation'
     ),
     @(
-        "Hyper-V",
+        'Hyper-V',
         { Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -ErrorAction Stop },
-        "hyperv"
+        'hyperv'
     ),
-    @("Már van telepítve VM")
+    @('Már van telepítve VM')
 )
 
-Write-Host "VM telepítése"
-Write-Host "Válassz ki egyet, vagy nyomj ENTER-t az alapértelmezetthez (VirtualBox)"
-0..($vms.Count - 1) | % { "{0}: {1}" -f $_, $vms[$_][0] }
+Write-Host 'VM telepítése'
+Write-Host 'Válassz ki egyet, vagy nyomj ENTER-t az alapértelmezetthez (VirtualBox)'
+0..($Vms.Count - 1) | % { '{0}: {1}' -f $_, $Vms[$_][0] }
 
 while ($true) {
     try {
-        $choice = Read-Host
-        $selectedVm = 0
-        if ($choice -ne "") {
-            $selectedVm = [int]::Parse($choice)
+        $Choice = Read-Host
+        $SelectedVm = 0
+        if ($Choice -ne '') {
+            $SelectedVm = [int]::Parse($Choice)
         }
-        if ($selectedVm -lt $vms.Count) {
-            if ($selectedVm -eq ($vms.Count - 1)) {
-                $selectedVm = -1
-                Write-Host "VM telepítás átugrása"
+        if ($SelectedVm -lt $Vms.Count) {
+            if ($SelectedVm -eq ($Vms.Count - 1)) {
+                $SelectedVm = -1
+                Write-Host 'VM telepítás átugrása'
                 break
             }
-            Write-Host "VM telepítése (ez sokáig eltarthat)"
-            & $vms[$selectedVm][1]
+            Write-Host 'VM telepítése (ez sokáig eltarthat)'
+            $Vms[$SelectedVm][1].Invoke()
             break
         }
     }
@@ -179,7 +161,7 @@ while ($true) {
     }
 }
 
-$bat = @"
+$Bat = @'
 @echo off
 
 set cwd=%cd%
@@ -191,34 +173,37 @@ cd /d %cwd%
 set cwd=
 set homesteadVagrant=
 
-"@
+'@
 Set-Location $env:windir
-(Write-Output $bat) | Out-FileUtf8NoBom "homestead.bat"
+(Write-Output $Bat) | Out-FileUtf8NoBom 'homestead.bat'
 
 if (Test-Path "$env:USERPROFILE\Homestead\" -PathType Container) {
-    Write-Host "Homestead már telepítve van"
-} else {
-    Write-Host "Homestead telepítése"
+    Write-Host 'Homestead már telepítve van'
+}
+else {
+    Write-Host 'Homestead telepítése'
     Set-Location $env:USERPROFILE
-    Start-Process mkdir Code -Wait
-    Start-Process git @("clone", "https://github.com/laravel/homestead.git", "Homestead") -Wait
+    Start-Process -Wait mkdir Code
+    Start-Process -Wait git clone, 'https://github.com/laravel/homestead.git', Homestead
     Set-Location Homestead
-    Start-Process git @("checkout", "release") -Wait
+    Start-Process -Wait git checkout, release
     & .\init.bat
 
-    if ($selectedVm -eq -1) {
-        Write-Host "Homestead.yaml fájlt neked kell szerkeszteni, mivel van már VM telepítve"
-    } elseif ($selectedVm -ne 0) {
-        $yaml = [string](Get-Content "Homestead.yaml")
-        $providerRe = [regex]"(provider:) \S+"
-        $providerRe.Replace($yaml, "`$1 $($vms[$selectedVm][2])", 1) | Out-FileUtf8NoBom "Homestead.yaml"
+    if ($SelectedVm -eq -1) {
+        Write-Host 'Homestead.yaml fájlt neked kell szerkeszteni, mivel van már VM telepítve'
+    }
+    elseif ($SelectedVm -ne 0) {
+        $Yaml = [string](Get-Content 'Homestead.yaml')
+        $ProviderRe = [regex]'(provider:) \S+'
+        $ProviderRe.Replace($Yaml, "`$1 $($Vms[$SelectedVm][2])", 1) | Out-FileUtf8NoBom 'Homestead.yaml'
     }
 
-    Write-Host "Vagrant Box els? futtatása..."
+    Write-Host 'Vagrant Box els? futtatása...'
     & vagrant provision
 }
 
-Write-Host @"
+Write-Host @'
+
 Kész! Pár hasznos parancs vagranthoz:
 
   homestead up
@@ -233,4 +218,5 @@ Kész! Pár hasznos parancs vagranthoz:
 
   homestead ssh
     Ezzel kapsz egy terminált a homestead virtuális gépbe
-"@
+
+'@
--- install.bat
+++ install.bat
@@ -2,9 +2,9 @@
 
 net session >nul 2>&1
 if %errorLevel% == 0 (
-    powershell -ExecutionPolicy Bypass -NoLogo -NoProfile -NoExit -file homestead.ps1
+    powershell -ExecutionPolicy Bypass -NoLogo -NoProfile -file homestead.ps1
 ) else (
     echo Ezt a fájlt adminisztrátorként kell futtatni!
 )
 
-pause >nul
+pause
