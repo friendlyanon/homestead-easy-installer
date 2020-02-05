41b61d2bc905518f1869f776dac52d86f569aac3
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-02-05 02:45:30 +0100

    Use ASCII characters only because cmd is broken

--- homestead.ps1
+++ homestead.ps1
@@ -1,5 +1,6 @@
-$OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
+#requires -version 3 -RunAsAdministrator
 
+$OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
 function Use-NewPath {
     $Path = @(
         [System.Environment]::GetEnvironmentVariable('Path', 'Machine'),
@@ -17,12 +18,16 @@ function Get-Document([string] $Uri) {
     return Invoke-WebRequest $Uri -UseBasicParsing
 }
 
-function Install-IfNecessary([string] $Command, [string] $Name, [scriptblock] $Installer) {
+function Install-IfNecessary {
+    param ([string] $Command, [string] $Name, [scriptblock] $Installer, [switch] $Long)
+
     if (Get-Command $Command -ErrorAction Ignore) {
-        Write-Output "$Name már telepítve van"
+        Write-Output "$Name mar telepitve van"
     }
     else {
+        Write-Host "$Name telepitese" + (if ($Long) { ' (ez sokaig eltarthat)' } else { '' })
         & $Installer
+        Use-NewPath
     }
 }
 
@@ -48,13 +53,11 @@ function Out-FileUtf8NoBom {
         [Parameter(ValueFromPipeline)] $InputObject
     )
 
-    #requires -version 3
-
     [System.IO.Directory]::SetCurrentDirectory($PWD)
     $LiteralPath = [IO.Path]::GetFullPath($LiteralPath)
 
     if ($NoClobber -and (Test-Path $LiteralPath)) {
-        Throw [IO.IOException] "The file '$LiteralPath' already exists."
+        throw [IO.IOException] "The file '$LiteralPath' already exists."
     }
 
     $Sw = New-Object IO.StreamWriter $LiteralPath, $Append
@@ -73,34 +76,22 @@ function Out-FileUtf8NoBom {
 }
 
 Install-IfNecessary choco Chocolatey {
-    Write-Host "$Name telepítése"
     Invoke-Expression ((Get-Downloader).DownloadString('https://chocolatey.org/install.ps1'))
-    Use-NewPath
 }
 
-Install-IfNecessary node Node {
-    Write-Host "$Name telepítése"
-    & choco install -y node
-    Use-NewPath
-}
+Install-IfNecessary node Node { & choco install -y node }
 
-Install-IfNecessary cmder 'cmder és UNIX eszközök' {
-    Write-Host "$Name telepítése"
-    & choco install -y cmder
-    Use-NewPath
-}
+Install-IfNecessary cmder 'cmder es UNIX eszkozok' { & choco install -y cmder }
 
-Install-IfNecessary vagrant Vagrant {
-    Write-Host "$Name telepítése (ez sokáig eltarthat)"
+Install-IfNecessary -Long vagrant Vagrant {
     $Link = Find (Get-Document 'https://www.vagrantup.com/downloads.html').Links `
     { $_.outerHTML.Contains('data-os="windows"') -and $_.outerHTML.Contains('data-arch="x86_64"') }
     Get-RemoteFile $Link.href 'vagrant.msi'
     Start-Process -Wait msiexec '/I', (Resolve-Path 'vagrant.msi')
     Remove-Item 'vagrant.msi'
-    Use-NewPath
 }
 
-Write-Host 'Hosts File Editor telepítése'
+Write-Host 'Hosts File Editor telepitese'
 Get-RemoteFile 'https://github.com/scottlerch/HostsFileEditor/releases/download/v1.2.0/HostsFileEditorSetup-1.2.0.msi' 'hfe.msi'
 Start-Process -Wait msiexec '/I', (Resolve-Path 'hfe.msi')
 Remove-Item 'hfe.msi'
@@ -131,11 +122,11 @@ $Vms = @(
         { Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -ErrorAction Stop },
         'hyperv'
     ),
-    @('Már van telepítve VM')
+    @('Mar van telepitve VM')
 )
 
-Write-Host 'VM telepítése'
-Write-Host 'Válassz ki egyet, vagy nyomj ENTER-t az alapértelmezetthez (VirtualBox)'
+Write-Host 'VM telepitese'
+Write-Host 'Valassz ki egyet, vagy nyomj ENTER-t az alapertelmezetthez (VirtualBox)'
 0..($Vms.Count - 1) | % { '{0}: {1}' -f $_, $Vms[$_][0] }
 
 while ($true) {
@@ -148,10 +139,10 @@ while ($true) {
         if ($SelectedVm -lt $Vms.Count) {
             if ($SelectedVm -eq ($Vms.Count - 1)) {
                 $SelectedVm = -1
-                Write-Host 'VM telepítás átugrása'
+                Write-Host 'VM telepites atugrasa'
                 break
             }
-            Write-Host 'VM telepítése (ez sokáig eltarthat)'
+            Write-Host 'VM telepitese (ez sokaig eltarthat)'
             $Vms[$SelectedVm][1].Invoke()
             break
         }
@@ -175,13 +166,13 @@ set homesteadVagrant=
 
 '@
 Set-Location $env:windir
-(Write-Output $Bat) | Out-FileUtf8NoBom 'homestead.bat'
+Write-Output $Bat | Out-FileUtf8NoBom 'homestead.bat'
 
 if (Test-Path "$env:USERPROFILE\Homestead\" -PathType Container) {
-    Write-Host 'Homestead már telepítve van'
+    Write-Host 'Homestead mar telepitve van'
 }
 else {
-    Write-Host 'Homestead telepítése'
+    Write-Host 'Homestead telepitese'
     Set-Location $env:USERPROFILE
     Start-Process -Wait mkdir Code
     Start-Process -Wait git clone, 'https://github.com/laravel/homestead.git', Homestead
@@ -190,7 +181,7 @@ else {
     & .\init.bat
 
     if ($SelectedVm -eq -1) {
-        Write-Host 'Homestead.yaml fájlt neked kell szerkeszteni, mivel van már VM telepítve'
+        Write-Host 'Homestead.yaml fajlt neked kell szerkeszteni, mivel van mar VM telepitve'
     }
     elseif ($SelectedVm -ne 0) {
         $Yaml = [string](Get-Content 'Homestead.yaml')
@@ -198,25 +189,25 @@ else {
         $ProviderRe.Replace($Yaml, "`$1 $($Vms[$SelectedVm][2])", 1) | Out-FileUtf8NoBom 'Homestead.yaml'
     }
 
-    Write-Host 'Vagrant Box els? futtatása...'
+    Write-Host 'Vagrant Box elso futtatasa...'
     & vagrant provision
 }
 
 Write-Host @'
 
-Kész! Pár hasznos parancs vagranthoz:
+Kesz! Par hasznos parancs vagranthoz:
 
   homestead up
-    Bootolja a homestead virtuális gépet
+    Bootolja a homestead virtualis gepet
 
   homestead up --provision
-    Homestead.yaml beli változtatások életbe léptetése
-    és a homestead virtuális gép bootolása
+    Homestead.yaml beli valtoztatasok eletbe leptetese
+    es a homestead virtualis gep bootolasa
 
   homestead halt
-    Kegyelmes módon leállítja a homestead virtuális gépet
+    Kegyelmes modon leallitja a homestead virtualis gepet
 
   homestead ssh
-    Ezzel kapsz egy terminált a homestead virtuális gépbe
+    Ezzel kapsz egy terminalt a homestead virtualis gepbe
 
 '@
--- install.bat
+++ install.bat
@@ -4,7 +4,7 @@ net session >nul 2>&1
 if %errorLevel% == 0 (
     powershell -ExecutionPolicy Bypass -NoLogo -NoProfile -file homestead.ps1
 ) else (
-    echo Ezt a fájlt adminisztrátorként kell futtatni!
+    echo Ezt a fajlt adminisztratorkent kell futtatni!
 )
 
 pause
